f6076228f58da522ef844dd2f537e7896759b905
Emmanuel Hateka <emmanuelhateka.com>
2022-02-20 00:21:39 +0000

    done with the manual part of the slider

---
+++ Auto_Manual_slider/app.js
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll('.slide');
+const btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
+
+// Manual Navigation
+const manualNav = manual => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+
+        btns.forEach((btn) => {
+            btn.classList.remove('active')
+        });
+
+    });
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+ }
+
+ btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+    })
+ });
