e7bb5873263109a6d425cb472051260bb3fbf10c
Emmanuel Hateka <emmanuelhateka.com>
2022-03-02 22:29:57 +0000

    almost done with add to cart. left with some small bug to be fixec

--- Add_to_cart/app.js
+++ Add_to_cart/app.js
@@ -1,7 +1,37 @@
 const cart = document.querySelector('.cart');
 const cartField = document.querySelector('.cart-field');
-const add = document.getElementsByClassName('.add');
+const add = document.getElementsByClassName('add');
 
 for (let but of add) {
-    
+    but.onclick = e=> {
+        let item = Number(cart.getAttribute('data-count') || 0);
+        cart.setAttribute('data-count', item + 1);
+        cart.classList.add('on');
+
+        // image animated to cart
+        let parent = e.target.parentNode.parentNode.parentNode;
+        let image = parent.querySelector('img');
+        let span = document.createElement('span');
+        span.className = 'image-carior';
+        parent.insertBefore(span, parent.lastElementChild);
+
+        let s_image = image.cloneNode(false);
+        span.appendChild(s_image);
+        span.classList.add('active');
+        setTimeout(() => {
+            span.classList.remove('active')
+            span.removeChild(s_image);
+        }, 500)
+
+        let clone = parent.cloneNode(true);
+        clone.querySelector('.add').style.display = "none";
+        clone.querySelector('.add').removeAttribute('class')
+        cartField.appendChild(clone)
+
+        if (clone) {
+            cart.onclick = () => {
+                cartField.classList.toggle('display');
+            }
+        }
+    }
 }
